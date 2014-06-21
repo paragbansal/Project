06bdf536d6b6d86565871e46270723e4afcbe9e5
paragbansal <paragbansalsbl@gmail.com>
2014-06-21 17:31:38 +0530

    Update tri.py

--- tri.py
+++ tri.py
@@ -7,16 +7,19 @@ import itertools
 import argparse
 
 from pyhull.delaunay import DelaunayTri
-from pyhull.convex_hull import ConvexHull
+
 
 import matplotlib.pyplot as plt
 import cv2
 a=[]
 img=cv2.imread('ball.jpg',0)
-edges = cv2.Canny(img,100,200)
-for i in range (0,200) :
-        for j in range(0,200):
-                if edges[i,j]==255 or (i%12==0 and i%20==0) :
+img = cv2.medianBlur(img,5) 
+th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
+            cv2.THRESH_BINARY,3,5)
+
+for i in range (0,th2.shape[0]) :
+        for j in range(0,th2.shape[1]):
+                if th2[i,j]==0 :
                         a.append((j,i))
 
 
@@ -25,9 +28,13 @@ for i in range (0,200) :
 
 b=np.asarray(a)
 
-legkeys = []
+for i in a :
+        del i
 
-legitems = []
+
+
+
+
 dim =2
 
 
@@ -35,29 +42,28 @@ dim =2
  #   p, = plt.plot(pt[0], pt[1], 'ro')
       
 
-#legkeys.append(p)
-#comment out everything below this
-legitems.append("Points")
+
+
+
 
 d = DelaunayTri(b)
-
+del b
 for s in d.simplices:
    for data in itertools.combinations(s.coords, dim):
         data = np.array(data)
         p, = plt.plot(data[:,0], data[:,1], 'g-')
 
-legkeys.append(p)
-legitems.append("Delaunay tri")
 
-d = ConvexHull(b)
+
+
+
 
 for s in d.simplices:
     for data in itertools.combinations(s.coords, dim):
         data = np.array(data)
         p, = plt.plot(data[:,0], data[:,1], 'b-')
+plt.imshow(cv2.Canny(img,200,200),cmap='gray')
 
-legkeys.append(p)
-legitems.append("Convex hull")
 plt.xticks([]), plt.yticks([])
 
 plt.show()
